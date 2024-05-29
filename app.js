c0ac4dab996f468e9e93377c9a054442ec825e7c
Julish <ottogys@gmail.com>
2024-05-29 18:15:14 +0200

    Add files via upload
    added some js functions, changed styles

--- app.js
+++ app.js
@@ -2,6 +2,47 @@ const button = document.getElementById("saveButton");
 let tg = window.Telegram.WebApp;
 tg.expand();
 
+
+const summaries = document.querySelectorAll('summary');
+
+summaries.forEach((summary) => {
+  summary.addEventListener('click', closeOpenedDetails);
+});
+
+function closeOpenedDetails() {
+  summaries.forEach((summary) => {
+    let detail = summary.parentNode;
+      if (detail != this.parentNode) {
+        detail.removeAttribute('open');
+      }
+    });
+}
+
+
+function fillingInputs(inputs){inputs.forEach(input => {
+		input.addEventListener('input', (event) =>{
+			const value = event.target.value;
+			const inputId = event.target.id[event.target.id.length-1]-1
+
+			for (let i = inputId; i < inputs.length; i++){
+				if (inputs[i] !== event.target) {
+					inputs[i].value = value;
+				}
+			}
+		})
+	})
+}
+
+
+document.addEventListener('DOMContentLoaded',(event) =>{
+	fillingInputs(document.querySelectorAll('.br'));
+	fillingInputs(document.querySelectorAll('.ln'));
+	fillingInputs(document.querySelectorAll('.sn'));
+	fillingInputs(document.querySelectorAll('.dn'));
+})
+
+
+
 function readingIt(elements) {
 	return Array.from(elements, element =>element.value);
 };
